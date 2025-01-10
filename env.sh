b3830b05024235d80742aa8b9025e5d8577ecab5
Cheng Tan <c.tan@northeastern.edu>
2025-01-10 14:38:59 -0500

    fix conflicting names

--- env.sh
+++ env.sh
@@ -3,13 +3,13 @@ export EGOS=$(pwd)
 # lab1-ex1:
 
 # # if you use MacOS, you should uncomment the following two lines:
-# export PATH=$PATH:$EGOS/riscv64-unknown-elf-gcc-8.3.0-2020.04.1-x86_64-apple-darwin/bin
-# export PATH=$PATH:$EGOS/riscv-qemu-5.2.0-2020.12.0-preview1-x86_64-apple-darwin/bin
+# export PATH=$EGOS/riscv64-unknown-elf-gcc-8.3.0-2020.04.1-x86_64-apple-darwin/bin:$PATH
+# export PATH=$EGOS/riscv-qemu-5.2.0-2020.12.0-preview1-x86_64-apple-darwin/bin:$PATH
 
 # # if you use Ubuntu, you should uncomment the following two lines:
-# export PATH=$PATH:$EGOS/riscv64-unknown-elf-gcc-8.3.0-2020.04.1-x86_64-linux-ubuntu14/bin
-# export PATH=$PATH:$EGOS/riscv-qemu-5.2.0-2020.12.0-preview1-x86_64-linux-ubuntu14/bin
+# export PATH=$EGOS/riscv64-unknown-elf-gcc-8.3.0-2020.04.1-x86_64-linux-ubuntu14/bin:$PATH
+# export PATH=$EGOS/riscv-qemu-5.2.0-2020.12.0-preview1-x86_64-linux-ubuntu14/bin:$PATH
 
 # # if you're using other OSes, please fill in XXX below
-# export PATH=$PATH:$EGOS/riscv64-unknown-elf-gcc-XXX/bin
-# export PATH=$PATH:$EGOS/riscv-qemu-XXX/bin
+# export PATH=$EGOS/riscv64-unknown-elf-gcc-XXX/bin:$PATH
+# export PATH=$EGOS/riscv-qemu-XXX/bin:$PATH
