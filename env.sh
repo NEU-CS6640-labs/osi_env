017fe39a294329657de2cb3fd1360af3a05fbe90
Cheng Tan <c.tan@northeastern.edu>
2026-01-10 17:48:05 -0500

    update for 26spring

--- env.sh
+++ env.sh
@@ -3,13 +3,13 @@ export EGOS=$(pwd)
 # lab1-ex1:
 
 # # if you use MacOS, you should uncomment the following two lines:
-# export PATH=$EGOS/riscv64-unknown-elf-gcc-8.3.0-2020.04.1-x86_64-apple-darwin/bin:$PATH
-# export PATH=$EGOS/riscv-qemu-5.2.0-2020.12.0-preview1-x86_64-apple-darwin/bin:$PATH
+# export PATH=$EGOS/xpack-riscv-none-elf-gcc-14.2.0-3/bin:$PATH
+# export PATH=$EGOS/xpack-qemu-riscv-8.2.2-1/bin:$PATH
 
 # # if you use Ubuntu, you should uncomment the following two lines:
-# export PATH=$EGOS/riscv64-unknown-elf-gcc-8.3.0-2020.04.1-x86_64-linux-ubuntu14/bin:$PATH
-# export PATH=$EGOS/riscv-qemu-5.2.0-2020.12.0-preview1-x86_64-linux-ubuntu14/bin:$PATH
+# export PATH=$EGOS/xpack-riscv-none-elf-gcc-14.2.0-3-linux-x64/bin:$PATH
+# export PATH=$EGOS/xpack-qemu-riscv-8.2.2-1-linux-x64/bin:$PATH
 
 # # if you're using other OSes, please fill in XXX below
-# export PATH=$EGOS/riscv64-unknown-elf-gcc-XXX/bin:$PATH
-# export PATH=$EGOS/riscv-qemu-XXX/bin:$PATH
+# export PATH=$EGOS/xpack-riscv-XXX/bin:$PATH
+# export PATH=$EGOS/xpack-qemu-XXX/bin:$PATH
